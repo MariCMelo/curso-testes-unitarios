a6a6300ce74a010f22a582dae66e62698dddc617
MariCMelo <marilia.correa.melo@gmail.com>
2023-10-04 15:45:10 -0300

    aula 5

--- aula04/tests/unit/calculator.test.ts
+++ aula04/tests/unit/calculator.test.ts
@@ -1,16 +1,39 @@
-import calculator from "calculator";
+import calculator from "../../src/calculator";
 
-describe("calculator tests", () => {
-  it("should work", async () => {
-    const sum = calculator.sum(10, 2);
-    const sub = calculator.sub(10, 2);
-    const div = calculator.div(10, 2);
-    const mul = calculator.mul(10, 2);
+// describe("calculator tests", () => {
+//   it("should sum two numbers", async () => {
+//     const sum = calculator.sum(10, 2);
+//     const sub = calculator.sub(10, 2);
+//     const div = calculator.div(10, 2);
+//     const mul = calculator.mul(10, 2);
     
   
-    expect(sum).toBe(12);
-    expect(sub).toBe(8);
-    expect(div).toBe(5);
-    expect(mul).toBe(20);
+//     expect(sum).toBe(12);
+//     expect(sub).toBe(8);
+//     expect(div).toBe(5);
+//     expect(mul).toBe(20);
+//   });
+// });
+
+describe("calculator tests", () => {
+  it("should sum two numbers", async () => {
+    const result = calculator.sum(2, 3)
+    expect(result).toBe(5);
   });
-});
+  it("should subtract two numbers", async () => {
+    const result = calculator.sub(2, 2)
+    expect(result).toBe(0);
+  });
+  it("should multiply two numbers", async () => {
+    const result = calculator.mul(2, 3)
+    expect(result).toBe(6);
+  });
+  it("should divide two numbers", async () => {
+    const result = calculator.div(10, 5)
+    expect(result).toBe(2);
+  });
+  it("should return 0 when diving by zero", async () => {
+    const result = calculator.div(10, 0)
+    expect(result).toBe(0);
+  });
+})
--- aula05/tests/unit/pacientes.test.ts
+++ aula05/tests/unit/pacientes.test.ts
@@ -1,5 +1,23 @@
-describe("calculator tests", () => {
+import { v4 as uuidv4 } from "uuid";
+import { generateProtocolForPacient } from "../../src/protocols-generator";
+
+jest.mock("uuid", () => {
+  return {
+    v4: () => {
+      return "mocked-uuid";
+    },
+  };
+});
+
+describe("generateProtocolForPacient", () => {
+  const result = generateProtocolForPacient("John", "Doe", true);
+
   it("should work", async () => {
-    expect(true).toBe(true);
+    expect(result).toEqual({
+      priority: true,
+      date: expect.any(Date),
+      pacient: "John Doe",
+      protocol: "mocked-uuid",
+    });
   });
 });
