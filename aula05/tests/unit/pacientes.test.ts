376a83b92eda7421526fbb8c0916c461037c9d6c
MariCMelo <marilia.correa.melo@gmail.com>
2023-10-04 23:45:04 -0300

    aulas 6 e 7

--- aula05/tests/unit/pacientes.test.ts
+++ aula05/tests/unit/pacientes.test.ts
@@ -1,5 +1,6 @@
-import { v4 as uuidv4 } from "uuid";
+import { faker } from "@faker-js/faker";
 import { generateProtocolForPacient } from "../../src/protocols-generator";
+
 
 jest.mock("uuid", () => {
   return {
@@ -9,15 +10,30 @@ jest.mock("uuid", () => {
   };
 });
 
-describe("generateProtocolForPacient", () => {
-  const result = generateProtocolForPacient("John", "Doe", true);
+// describe("generateProtocolForPacient", () => {
+//   const result = generateProtocolForPacient("John", "Doe", true);
 
-  it("should work", async () => {
-    expect(result).toEqual({
+//   it("should work", async () => {
+//     expect(result).toEqual({
+//       priority: true,
+//       date: expect.any(Date),
+//       pacient: "John Doe",
+//       protocol: "mocked-uuid",
+//     });
+//   });
+// });
+
+describe("Protocol Generator Tests", () => {
+  it("should create an protocol", async () => {
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+
+    const protocol = generateProtocolForPacient(firstName, lastName, true);
+    expect(protocol).toEqual({
       priority: true,
       date: expect.any(Date),
-      pacient: "John Doe",
-      protocol: "mocked-uuid",
+      pacient: `${firstName} ${lastName}`,
+      protocol: "mock protocol"
     });
   });
 });
